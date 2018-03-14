a42676c790762c39c6d68625b72301cd8007e76d
huanglifeng <hfeeki@outlook.com>
2018-03-14 10:10:58 +0800

    now "docker-compose up" working well, and do not need parity-fix and parity-system in docker.

--- docker/parity-fix/docker-entrypoint.sh
+++ docker/parity-fix/docker-entrypoint.sh
@@ -2,22 +2,4 @@
 
 set -e
 
-mkdir -p /opt/parity
-
-cat > /opt/parity/parity-fix.conf <<-EOF
-fix {
-  address        = 0.0.0.0
-  port           = 4010
-  sender-comp-id = parity
-}
-
-order-entry {
-  address = ${PARITY_FIX_ORDER_ENTRY_ADDRESS:-"parity-system"}
-  port    = ${PARITY_FIX_ORDER_ENTRY_PORT:-"4000"}
-}
-EOF
-
-exec cp /parity/parity-fix.jar /opt/parity/parity-fix.jar
-#exec cp /parity/parity-fix.conf /opt/parity/parity-fix.conf
-exec chmod 644 /opt/parity/parity-fix.jar
 exec /usr/bin/java -jar /opt/parity/parity-fix.jar /opt/parity/parity-fix.conf
--- docker/parity-system/docker-entrypoint.sh
+++ docker/parity-system/docker-entrypoint.sh
@@ -1,33 +1,31 @@
 #!/bin/sh
 
-mkdir -p /opt/parity
 
-cat > /opt/parity/parity-system.conf <<-EOF
-market-data {
-  session             = parity
-  multicast-interface = eth0
-  multicast-group     = 224.0.0.1
-  multicast-port      = 5000
-  request-port        = 5001
-}
+#cat > /opt/parity/parity-system.conf <<-EOF
+#market-data {
+#  session             = parity
+#  multicast-interface = eth0
+#  multicast-group     = 224.0.0.1
+#  multicast-port      = 5000
+#  request-port        = 5001
+#}
+#
+#market-report {
+#  session             = parity
+#  multicast-interface = eth0
+#  multicast-group     = 224.0.0.1
+#  multicast-port      = 6000
+#  request-port        = 6001
+#}
+#
+#order-entry {
+#  address = 0.0.0.0
+#  port    = 4000
+#}
+#
+#instruments = [ ${PARITY_SYSTEM_INSTRUMENTS:-"FOO,BAR,BAZ"} ]
+#EOF
 
-market-report {
-  session             = parity
-  multicast-interface = eth0
-  multicast-group     = 224.0.0.1
-  multicast-port      = 6000
-  request-port        = 6001
-}
 
-order-entry {
-  address = 0.0.0.0
-  port    = 4000
-}
-
-instruments = [ ${PARITY_SYSTEM_INSTRUMENTS:-"FOO,BAR,BAZ"} ]
-EOF
-
-exec cp /parity/parity-system.jar /opt/parity/parity-system.jar
-#exec cp /parity/parity-system.conf /opt/parity/parity-system.conf
 exec chmod 644 /opt/parity/parity-system.jar
 exec /usr/bin/java -jar /opt/parity/parity-system.jar /opt/parity/parity-system.conf
